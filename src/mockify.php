4af811f932d622adb211178aa2612d86827b7a18
Luke Green <luke.green@therapybrands.com>
2021-05-19 14:05:49 -0500

    flagify

--- src/mockify.php
+++ src/mockify.php
@@ -1,9 +1,260 @@
 <?php
 
-$text = $_POST['text'];
+$text = 'flagify more money';
 const MAX_CONSECUTIVE_TYPE = 3;
 // $text = "t e s t i n g a l o n g e r s e n t e n c e!";
 // var_dump($text); die;
+$FLAG_CODES = [
+  'AD',
+  'AE',
+  'AF',
+  'AG',
+  'AI',
+  'AL',
+  'AM',
+  'AO',
+  'AQ',
+  'AR',
+  'AS',
+  'AT',
+  'AU',
+  'AW',
+  'AX',
+  'AZ',
+  'BA',
+  'BB',
+  'BD',
+  'BE',
+  'BF',
+  'BG',
+  'BH',
+  'BI',
+  'BJ',
+  'BL',
+  'BM',
+  'BN',
+  'BO',
+  'BQ',
+  'BR',
+  'BS',
+  'BT',
+  'BV',
+  'BW',
+  'BY',
+  'BZ',
+  'CA',
+  'CC',
+  'CD',
+  'CF',
+  'CG',
+  'CH',
+  'CI',
+  'CK',
+  'CL',
+  'CM',
+  'CN',
+  'CO',
+  'CR',
+  'CU',
+  'CV',
+  'CW',
+  'CX',
+  'CY',
+  'CZ',
+  'DE',
+  'DJ',
+  'DK',
+  'DM',
+  'DO',
+  'DZ',
+  'EC',
+  'EE',
+  'EG',
+  'EH',
+  'ER',
+  'ES',
+  'ET',
+  'FI',
+  'FJ',
+  'FK',
+  'FM',
+  'FO',
+  'FR',
+  'GA',
+  'GB',
+  'GD',
+  'GE',
+  'GF',
+  'GG',
+  'GH',
+  'GI',
+  'GL',
+  'GM',
+  'GN',
+  'GP',
+  'GQ',
+  'GR',
+  'GS',
+  'GT',
+  'GU',
+  'GW',
+  'GY',
+  'HK',
+  'HM',
+  'HN',
+  'HR',
+  'HT',
+  'HU',
+  'ID',
+  'IE',
+  'IL',
+  'IM',
+  'IN',
+  'IO',
+  'IQ',
+  'IR',
+  'IS',
+  'IT',
+  'JE',
+  'JM',
+  'JO',
+  'JP',
+  'KE',
+  'KG',
+  'KH',
+  'KI',
+  'KM',
+  'KN',
+  'KP',
+  'KR',
+  'KW',
+  'KY',
+  'KZ',
+  'LA',
+  'LB',
+  'LC',
+  'LI',
+  'LK',
+  'LR',
+  'LS',
+  'LT',
+  'LU',
+  'LV',
+  'LY',
+  'MA',
+  'MC',
+  'MD',
+  'ME',
+  'MF',
+  'MG',
+  'MH',
+  'MK',
+  'ML',
+  'MM',
+  'MN',
+  'MO',
+  'MP',
+  'MQ',
+  'MR',
+  'MS',
+  'MT',
+  'MU',
+  'MV',
+  'MW',
+  'MX',
+  'MY',
+  'MZ',
+  'NA',
+  'NC',
+  'NE',
+  'NF',
+  'NG',
+  'NI',
+  'NL',
+  'NO',
+  'NP',
+  'NR',
+  'NU',
+  'NZ',
+  'OM',
+  'PA',
+  'PE',
+  'PF',
+  'PG',
+  'PH',
+  'PK',
+  'PL',
+  'PM',
+  'PN',
+  'PR',
+  'PS',
+  'PT',
+  'PW',
+  'PY',
+  'QA',
+  'RE',
+  'RO',
+  'RS',
+  'RU',
+  'RW',
+  'SA',
+  'SB',
+  'SC',
+  'SD',
+  'SE',
+  'SG',
+  'SH',
+  'SI',
+  'SJ',
+  'SK',
+  'SL',
+  'SM',
+  'SN',
+  'SO',
+  'SR',
+  'SS',
+  'ST',
+  'SV',
+  'SX',
+  'SY',
+  'SZ',
+  'TC',
+  'TD',
+  'TF',
+  'TG',
+  'TH',
+  'TJ',
+  'TK',
+  'TL',
+  'TM',
+  'TN',
+  'TO',
+  'TR',
+  'TT',
+  'TV',
+  'TW',
+  'TZ',
+  'UA',
+  'UG',
+  'UM',
+  'US',
+  'UY',
+  'UZ',
+  'VA',
+  'VC',
+  'VE',
+  'VG',
+  'VI',
+  'VN',
+  'VU',
+  'WF',
+  'WS',
+  'YE',
+  'YT',
+  'ZA',
+  'ZM',
+  'ZW',
+];
 
 function clapify($text) {
   $first = explode(" ", $text)[0];
@@ -15,7 +266,10 @@ function shopify($text) {
   return $first === 'shopify';
 }
 
-
+function flagify($text) {
+  $first = explode(" ", $text)[0];
+  return $first === 'flagify';
+}
 
 if (clapify($text)) {
   
@@ -29,6 +283,45 @@ else if (shopify($text)) {
   $deShopifiedString = substr($text, 8);
   $returnText = 'lettuce turnip the beets';
   
+}
+else if (flagify($text)) {
+  $text = substr($text, 8);
+  function mkflag($chunk, $FLAG_CODES) {
+    if (in_array(strtoupper($chunk), $FLAG_CODES)) {
+      return [
+        ':flag-'.strtolower($chunk).':',
+        true
+      ];
+    } else {
+      return [
+        ':alphabet-white-'.strtolower(substr($chunk,0,1)).':',
+        false
+      ];
+    }
+  }
+
+  $count = 0;
+  $message = '';
+  while($count < strlen($text)) {
+    if (!ctype_alpha(substr($text, $count, 1))) {
+      $message .= ' ';
+      $count++;
+    }
+    else if ($count + 2 < strlen($text)) {
+      $res = mkflag(substr($text, $count, 2), $FLAG_CODES);
+      if ($res[1]) {
+        $count += 2;
+      } else {
+        $count++;
+      }
+      $message .= $res[0];
+    } else {
+      $message .= ':alphabet-white-'.substr($text, $count, 1).':';
+      $count++;
+    }
+  }
+
+  $returnText = $message;
 }
 else {
  
